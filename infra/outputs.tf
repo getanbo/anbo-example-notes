d41505b88862c6a19cd16e270da45c6a6673abaf
Sung Eun Choi <sungeunchoi.1234@gmail.com>
2026-07-15 13:01:08 +0100

    fix: persist Secrets Manager resource identifier

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -22,7 +22,7 @@ output "state_machine_arn" {
   value = aws_sfn_state_machine.notes.arn
 }
 
-output "secret_arn" {
+output "application_config_arn" {
   value = aws_secretsmanager_secret.application.arn
 }
 
