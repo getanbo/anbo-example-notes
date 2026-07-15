6870304645837da3395739afaa0962856463c9e1
Sung Eun Choi <sungeunchoi.1234@gmail.com>
2026-07-15 13:07:47 +0100

    test: retain Terraform output lifecycle coverage

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -22,7 +22,7 @@ output "state_machine_arn" {
   value = aws_sfn_state_machine.notes.arn
 }
 
-output "application_config_arn" {
+output "secret_arn" {
   value = aws_secretsmanager_secret.application.arn
 }
 
